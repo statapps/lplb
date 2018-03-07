5063b5a4a9ade2cd91cbf7d79ff567c2d1d82bd9
Bingshu Chen <Bingshu.Chen@queensu.ca>
2018-03-07 15:59:16 -0500

    update

--- R/lple_boot.R
+++ R/lple_boot.R
@@ -3,13 +3,13 @@ lple_boot = function(object,conf.int = 0.95) {
   y = object$y
   control = object$control
   B = control$B
-  pb = rep(0, B)
+  bp = rep(0, B)
   beta_w = object$beta_w
         
   boot_beta = matrix(0, B, length(beta_w))
-  cat('\nBootstraping')
+  cat('\nBootstraping...')
   for(i in 1:B) {
-    cat('.')
+    if(B>30 & (i%%10 == 0)) cat('.')
     idx = sample(1:n, n, replace = TRUE)
     datb = dat[idx, ]
     Xb = cbind(datb$z, datb$w)
@@ -20,13 +20,14 @@ lple_boot = function(object,conf.int = 0.95) {
     fitb = lple_fit(Xb, yb, control)
     beta_b = as.vector(fitb$beta_w)
     sd_b = as.vector(fitb$sd)
-    pb[i] = max(abs((beta_b - beta_w)/sd_b))
+    bp[i] = max(abs((beta_b - beta_w)/sd_b))
     boot_beta[i, ] = beta_b
 				  }
     boot_se = apply(boot_beta, 2, sd)
+    boot_bias = apply(boot_beta, 2, mean) - beta_w
     qbp = quantile(pb, conf.int)
-    
+ 
     cat('\nDone! Bootstrap scb (p) = ', qbp)
-    return(list(boot_beta = boot_beta, boot_se = boot_se, boot_p = pb, qbp = qbp))
+    return(list(boot_beta = boot_beta, bias = boot_bias, se = boot_se, boot_p = bp, qbp = qbp))
 }
 
