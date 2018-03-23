068006e36df1bd1b81ed5d1d05870b9902b56768
Bingshu Chen <Bingshu.Chen@queensu.ca>
2018-03-23 10:55:35 -0400

    add asymptotic scb

--- R/lple.R
+++ R/lple.R
@@ -86,3 +86,64 @@ plot.lple = function(x, ..., scale = c('original', 'transformed')) {
   }
 }
 
+
+##########LPLE fit was checked aganist the following code and get correct results. 
+.coxlple = function(X, y, weights) {
+  b0 = rep(0, length(X[1, ]))
+  ds = 1
+  while(ds > 1e-10) {
+    sc = .wscore(X, y, b0, weights)
+    b0 = b0 + sc 
+    ds = sum(abs(sc))
+  }
+  b0
+}
+
+.wscore = function(X, y, beta, weights) {
+  status = y[, 2]
+  lp = X%*%beta
+  exb = exp(lp)
+  n = length(status)
+
+  ## Sm is a upper triangular matrix of 1
+  Sm = matrix(1, n, n)
+  Sm[lower.tri(Sm)] = 0
+       
+  rcumsum <- function(x) rev(cumsum(rev(x)))
+  s0 = rcumsum(exb*weights)
+  s1 = Sm%*%((X)*as.vector(exb*weights))
+  sc = (X-s1/s0)*as.vector(weights*status)
+  V = t(sc)%*%sc
+  return(solve(V)%*%t((weights*status)%*%(X-s1/s0)))
+}
+
+
+##############Asymptotic SCB    
+.dvn = function(h, kn) {
+  ### asymptotic distribution
+  A = 5
+  ds = 0.001
+  s = seq(-A, A, ds)
+  
+  #normal kernel
+  ks = K_func(s, 0, 1, kernel=kn)
+  
+  ks1=c(0, ks)
+  ks2 = c(ks, 0)
+  #derivative of kernel
+  dks = (ks2-ks1)/ds
+  
+  # integration of v0 = k(w)^2 and v1 = k'(w)^2
+  v0 = mean(ks^2)*2*A
+  v1 = mean(dks^2)*2*A
+  #cat('v0=', v0, 'v1 = ', v1, '\n')
+  dvn = (-2*log(h))^0.5 + (-2*log(h))^-0.5*log(v1/(4*pi*v0))
+  return(dvn)
+}
+#xa = -log(-log(a)/2)
+#p=(-2*log(h))^(-0.5) * log((2*pi*log(a^(-0.5)))^-1 ) +  (-2*log(h))^(0.5)
+
+asymSCB = function(h, kernel, conf.int = 0.95){
+  dn = .dvn(h, kernel)
+  pn = (dn + (log(2)-log(-log(conf.int)))*(-2*log(h))^-0.5)
+}
