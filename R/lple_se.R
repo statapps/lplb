981523aa41052e6d7afea9a7d789f67cded99a37
Bingshu Chen <Bingshu.Chen@queensu.ca>
2018-04-14 16:25:12 -0400

    up

--- R/lple_se.R
+++ R/lple_se.R
@@ -1,53 +1,63 @@
 
-Sk2g = function(X, y, control, bw0, w0, Sm, haz) {
+Sk2g = function(X, y, control, bw0, w0, haz) {
   kernel = control$kernel
   status = y[, 2]
   h = control$h
-  w_est = control$w_est
+  #w_est = control$w_est
   p1 = control$p1
   X_R = interaction_X_w0(X, p1, w0)
   p = ncol(X) - 1
-  p_fai = p + p1 + 1
+  p2 = p + p1 + 1 #p2 total vector length for fai
   n = nrow(X)
   w = X[, ncol(X)]
-  xi = as.matrix(bw0)
+  bw = as.matrix(bw0)
   
   R = t(X_R)
-  H = diag(rep(c(1, h, h), c(p, p1, 1)), p_fai, p_fai)
-  H1 = diag(rep(c(1, 1/h, 1/h), c(p, p1, 1)), p_fai, p_fai)
-  fai = H  %*% xi
-  Tx  = H1 %*% R
+  H = diag(rep(c(1, h, h), c(p, p1, 1)), p2, p2)
+  H1 = diag(rep(c(1, 1/h, 1/h), c(p, p1, 1)), p2, p2)
+  fai = H  %*% bw
+  U   = t(H1 %*% R)
   
   kh = K_func(w, w0, h, kernel)
-  ezb_fai = as.vector(exp(t(Tx) %*% fai)) * kh
+  ezb_fai = as.vector(exp(U %*% fai)) * kh
   
-  Sf0 = Sm %*% ezb_fai
-  Sf1 = Sm %*% (t(Tx) * ezb_fai)
-  
-  TSf1 = t(Tx) - Sf1/c(Sf0)
+  #Sm = matrix(1, n, n)
+  #Sm[lower.tri(Sm)] = 0
+  #Sf0 = Sm %*% ezb_fai
+  #Sf1 = Sm %*% (U * ezb_fai)
+
+  # this code is faster
+  Sf0 = rcumsum(ezb_fai)  #reverse cumsum
+  ubz = U*ezb_fai
+  Sf1 = apply(ubz, 2, rcumsum)
+
+  TSf1 = U - Sf1/c(Sf0)
   
   TSf1_2 = apply(TSf1, 1, function(x) {return(x %*% t(x))})
-  TSf2   = aperm(array(TSf1_2, c(p_fai, p_fai, n)), c(3, 1, 2))
+  TSf2   = aperm(array(TSf1_2, c(p2, p2, n)), c(3, 1, 2))
 
-  pi_fai = colSums(status * (TSf2*kh^2), dims = 1)
-  B_n    = colSums(kh*TSf1*haz,   dims = 1)
+  Pi_n = colSums(status * (TSf2*kh^2), dims = 1)
+  B_n  = colSums(kh*TSf1*haz,   dims = 1)
   
-  Zt_fai = apply(t(Tx), 1, function(x) {return(x %*% t(x))})
-  Z2_fai = array(Zt_fai, c(p_fai, p_fai, n))
-  Z2ezb_fai = Z2_fai * array(rep(ezb_fai, each = p_fai * p_fai), 
-                          c(p_fai, p_fai, n))
-  Sf2 = apply(Z2ezb_fai, c(1, 2), function(x, y) {return(y %*% x)}, Sm)
+  Zt_fai = apply(U, 1, function(x) {return(x %*% t(x))})
+  Z2_fai = array(Zt_fai, c(p2, p2, n))
+  Z2ezb_fai = Z2_fai * array(rep(ezb_fai, each = p2 * p2), c(p2, p2, n))
+
+  #code with reverse cumsum (rcumsum) runs faster
+  #Sf2 = apply(Z2ezb_fai, c(1, 2), function(x, y) {return(y %*% x)}, Sm)
+  #print(Sf2[1, , ])
+  Sf2 = apply(Z2ezb_fai, c(1, 2), rcumsum)
+  #print(Sf2[1, , ])
   Sf1_2 = aperm(array(apply(Sf1, 1, function(x) { return(x %*% t(x))}), 
-                    c(p_fai, p_fai, n)), c(3, 1, 2))
+                    c(p2, p2, n)), c(3, 1, 2))
   A_n = colSums(status * (kh * (Sf2/c(Sf0) - Sf1_2/c(Sf0)^2)), dims = 1)
-  ###I_fai is the same as An in the paper
-  return(list(A_n = A_n, pi_fai = pi_fai, B_n = B_n))
+  return(list(A_n = A_n, Pi_n = Pi_n, B_n = B_n))
 }
 
 lple_se = function(X, y, control, betaw, gw){
   h = control$h
-  w_est = control$w_est
-  m = length(w_est)
+  w = control$w_est
+  m = length(w)
   p1 = control$p1
   n = nrow(X)
   p = ncol(X)
@@ -66,24 +76,21 @@ lple_se = function(X, y, control, betaw, gw){
   haz0= nevent/rxb       #hazard function
   haz = exb*haz0
 
-  Sm = matrix(1, n, n)
-  Sm[lower.tri(Sm)] = 0
   mtrx2 = matrix(0, nrow = p1, ncol = p + p1)
   for (i in 1:p1) mtrx2[i, i] = 1
   
   sd.err = matrix(0, nrow = m, ncol = p1)
   bias = sd.err
   for (i in 1:m) {
-    w0 = w_est[i]
+    w0 = w[i]
     bw0 = betaw[i, ]
-    skf = Sk2g(X, y, control, bw0, w0, Sm, haz)
+    skf = Sk2g(X, y, control, bw0, w0, haz)
     A_n = skf$A_n
     A1  = solve(A_n)
-    pi_fai = skf$pi_fai
-    gamma = A1 %*% pi_fai %*% A1 
+    Pi_n = skf$Pi_n
+    gamma = A1 %*% Pi_n %*% A1 
     gamma11 = mtrx2 %*% gamma %*% t(mtrx2)
     sd.err[i, ] = sqrt(diag(gamma11))
-    #print(solve(pi_fai)%*%t(skf$B_fai))
     bias[i, ] = (A1%*%skf$B_n)[1:p1]
   }
   return(list(sd.err = sd.err, bias = bias, haz = haz0))
